74575b7311a06a027f721667622b34cbbdabedf0
Dennimir <dennimir@gmail.com>
2022-04-03 20:50:59 +0300

    slice part

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	bookings := []string{}
+	var userFirstName string
+	var userLastName string
+	var userEmail string
+	var userTickets uint8
 	const conferenceName string = "Go Conference"
 	const conferenceTickets uint8 = 50
 	remainingtickets := conferenceTickets
@@ -11,11 +16,22 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingtickets)
 	fmt.Println("Get your ticket here to attend")
 
-	var userName string
-	var userTickets int
-	//ask user for the name
-	userName = "Tom"
-	userTickets = 1
-	fmt.Print(userName, userTickets)
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&userFirstName)
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&userLastName)
+	fmt.Println("Enter your email:")
+	fmt.Scan(&userEmail)
+	fmt.Printf("%v, how many tickets do you want to book:\n", userFirstName)
+	fmt.Scan(&userTickets)
+
+	remainingtickets = remainingtickets - userTickets
+	bookings = append(bookings, userFirstName+" "+userLastName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.\nYou will receive a confirmation email at %v\n",
+		userFirstName, userLastName, userTickets, userEmail)
+	fmt.Printf("%v tickets remaining for \"%v\"\n", remainingtickets, conferenceName)
 
 }
